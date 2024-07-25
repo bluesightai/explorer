949c825246495c757c7e8040d53b5c1a0772e87a
Maria Rybakowska <maria.rybakowska@niceproject.eu>
2024-07-25 21:04:14 +0200

    Merge

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import App from "./App.tsx"
-import "./index.scss"
+import MapComponent from "./components/MapComponent.tsx"
+import "./main.scss"
 import React from "react"
 import ReactDOM from "react-dom/client"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <div className="App" style={{ width: "100vw", height: "100vh" }}>
+      <MapComponent />
+    </div>
   </React.StrictMode>,
 )
