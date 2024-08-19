7eaa968bcef433791eb38661ccad6646d2ba66d4
Alexander Smirnov <alex@asmirnov.xyz>
2024-08-19 14:10:09 +0100

    prettier

--- src/components/MapComponent.tsx
+++ src/components/MapComponent.tsx
@@ -1,10 +1,10 @@
+import { Config, get_style_url, mapboxToken } from "../config"
 import { useAppState } from "../hooks/AppContext"
 import { useBoundingBoxes } from "../hooks/useBoundingBoxes"
 import { useMapState } from "../hooks/useMapState"
 import { usePinning } from "../hooks/usePinning"
 import { calculateCenterAndZoom, isPointInCalifornia } from "../utils/mapUtils"
 import ClusteringCard from "./clusteringcard/ClusteringCard"
-import { Config, get_style_url, mapboxToken } from "../config"
 import ControlWidget from "./control/ControlWidget"
 import SearchBox from "./input/SearchBox"
 import { createMapLayers } from "./layers/layers"
@@ -19,8 +19,6 @@ function selectByIndices<T extends { id: number }>(list: T[], indices: number[])
   return list.filter((item) => indices.includes(item.id))
 }
 
-
-
 function DeckGLOverlay(props: DeckProps) {
   // @ts-ignore
   const overlay = useControl<MapboxOverlay>(() => new MapboxOverlay(props))
@@ -34,7 +32,6 @@ export default function MapComponent() {
   const { state, dispatch } = useAppState()
 
   const { viewState, setViewState, popupInfo, setPopupInfo } = useMapState()
-
 
   const { handleFetchBoundingBoxes, handleFindSimilar } = useBoundingBoxes()
 
@@ -69,21 +66,21 @@ export default function MapComponent() {
   const layers =
     state.mode.type == "image"
       ? createMapLayers(
-        state.visibleBoundingBoxes
-          ? selectByIndices(state.mode.targetBoundingBoxes, state.visibleBoundingBoxes)
-          : state.mode.targetBoundingBoxes,
-        state.visibleBoundingBoxes
-          ? selectByIndices(state.resultBoundingBoxes, state.visibleBoundingBoxes)
-          : state.resultBoundingBoxes,
-        viewState.zoom,
-      )
+          state.visibleBoundingBoxes
+            ? selectByIndices(state.mode.targetBoundingBoxes, state.visibleBoundingBoxes)
+            : state.mode.targetBoundingBoxes,
+          state.visibleBoundingBoxes
+            ? selectByIndices(state.resultBoundingBoxes, state.visibleBoundingBoxes)
+            : state.resultBoundingBoxes,
+          viewState.zoom,
+        )
       : createMapLayers(
-        [],
-        state.visibleBoundingBoxes
-          ? selectByIndices(state.resultBoundingBoxes, state.visibleBoundingBoxes)
-          : state.resultBoundingBoxes,
-        viewState.zoom,
-      )
+          [],
+          state.visibleBoundingBoxes
+            ? selectByIndices(state.resultBoundingBoxes, state.visibleBoundingBoxes)
+            : state.resultBoundingBoxes,
+          viewState.zoom,
+        )
 
   const handleSearchAndCancelPin = useCallback(() => {
     handleFindSimilar()
@@ -125,18 +122,16 @@ export default function MapComponent() {
         </Popup>
       )}
 
-      <ControlWidget setViewState={(config: Config) => {
-
-        setViewState((prevState) => ({
-          ...prevState,
-          latitude: config.initial_lat,
-          longitude: config.initial_lon,
-
-        }))
-        handleCleanSearch()
-
-      }
-      } />
+      <ControlWidget
+        setViewState={(config: Config) => {
+          setViewState((prevState) => ({
+            ...prevState,
+            latitude: config.initial_lat,
+            longitude: config.initial_lon,
+          }))
+          handleCleanSearch()
+        }}
+      />
 
       <SceneCard
         onTileClick={handleTileClick}
