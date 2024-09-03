78f9c2c9a9f1140c28465ad8c101aefbb1f2d801
Szymon Zmyslony <szymon.zmyslony@gmail.com>
2024-09-02 18:47:20 -0700

    Add contact info

--- src/components/HelpTour.tsx
+++ src/components/HelpTour.tsx
@@ -4,16 +4,16 @@ import React from "react"
 // Make sure to create this CSS file
 
 interface HelpButtonProps {
-    onClick: (event: React.MouseEvent<HTMLButtonElement>) => void
-    className?: string
+  onClick: (event: React.MouseEvent<HTMLButtonElement>) => void
+  className?: string
 }
 
 const HelpButton: React.FC<HelpButtonProps> = ({ onClick, className = "" }) => {
-    return (
-        <button onClick={onClick} className={`help-button ${className}`}>
-            <span className="help-button__icon">Help &#10067;</span>
-        </button>
-    )
+  return (
+    <button onClick={onClick} className={`help-button ${className}`}>
+      <span className="help-button__icon">Help &#10067;</span>
+    </button>
+  )
 }
 
 export default HelpButton
