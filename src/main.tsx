90017809edf036dd93996806acc1b760f1631c76
Alexander Smirnov <alex@asmirnov.xyz>
2024-09-11 16:48:29 -0300

    comment out desktop version to allow building

--- src/main.tsx
+++ src/main.tsx
@@ -1,39 +1,40 @@
 import MapApp from "./components/MainMap/MapApp"
 import { AppProvider } from "./hooks/AppContext"
 import "./main.scss"
-import React, { useEffect, useState } from "react"
+// import React, { useEffect, useState } from "react"
+import React from "react"
 import ReactDOM from "react-dom/client"
 
-const DesktopOnlyApp: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  const [isMobile, setIsMobile] = useState(false)
+// const DesktopOnlyApp: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+//   const [isMobile, setIsMobile] = useState(false)
 
-  useEffect(() => {
-    const checkScreenSize = () => {
-      setIsMobile(window.innerWidth <= 768) // Adjust this breakpoint as needed
-    }
+//   useEffect(() => {
+//     const checkScreenSize = () => {
+//       setIsMobile(window.innerWidth <= 768) // Adjust this breakpoint as needed
+//     }
 
-    checkScreenSize()
-    window.addEventListener("resize", checkScreenSize)
+//     checkScreenSize()
+//     window.addEventListener("resize", checkScreenSize)
 
-    return () => window.removeEventListener("resize", checkScreenSize)
-  }, [])
+//     return () => window.removeEventListener("resize", checkScreenSize)
+//   }, [])
 
-  if (isMobile) {
-    return (
-      <div className="mobile-message-container">
-        <div className="mobile-message">
-          <h1 className="mobile-message-title">Desktop Only</h1>
-          <p className="mobile-message-text">
-            We're sorry, but this map application is only available on desktop devices.
-          </p>
-          <p className="mobile-message-text">Please visit us on a larger screen for the best experience.</p>
-        </div>
-      </div>
-    )
-  }
+//   if (isMobile) {
+//     return (
+//       <div className="mobile-message-container">
+//         <div className="mobile-message">
+//           <h1 className="mobile-message-title">Desktop Only</h1>
+//           <p className="mobile-message-text">
+//             We're sorry, but this map application is only available on desktop devices.
+//           </p>
+//           <p className="mobile-message-text">Please visit us on a larger screen for the best experience.</p>
+//         </div>
+//       </div>
+//     )
+//   }
 
-  return <>{children}</>
-}
+//   return <>{children}</>
+// }
 
 const App: React.FC = () => {
   return (
